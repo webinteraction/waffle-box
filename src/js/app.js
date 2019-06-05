2add0add53cf674a5b7ad755d65528bfb4e0370d
kyleschaeffer <kyle@spirito.us>
2019-06-05 13:45:10 -0400

    Add modal dialog component and styles

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,14 @@ const dropdowns = new Toggle({
   single: true,
   keyControl: true,
 })
+
+// Modals
+const modals = new Toggle({
+  attr: 'data-modal',
+  toggleContainer: '.modal',
+  blur: true,
+  single: true,
+  onToggle: (btn, target, isToggled) => {
+    document.body.classList[isToggled ? 'add' : 'remove']('has-modal')
+  },
+})
