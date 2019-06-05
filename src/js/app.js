d64a0864e1f7ac4f8f6e6364da15eecfc1e7c3bb
kyleschaeffer <kyle@spirito.us>
2019-06-05 09:45:48 -0400

    Add responsive menu toggle

--- src/js/app.js
+++ src/js/app.js
@@ -5,4 +5,12 @@ const dropdowns = new Toggle({
   attr: 'data-dropdown',
   toggleContainer: '.dropdown',
   blur: true,
+  single: true,
+})
+
+// Toggle menu
+const toggleMenu = new Toggle({
+  attr: 'data-menu-toggle',
+  toggleContainer: '.menu-toggle',
+  toggleClass: 'active-sm',
 })
